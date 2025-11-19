49d9cb601bbc4d6bf8653b6b403669281e298ebc
Mazun Volodimir <mazunvolodimir777@gmail.com>
2025-11-19 23:07:49 +0200

    crud

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -4,11 +4,17 @@ import { Router } from 'express';
 import {
   getStudents,
   getStudentById,
+  createStudent,
+  deleteStudent,
+  updateStudent,
 } from '../controllers/studentsController.js';
 
 const router = Router();
 
 router.get('/students', getStudents);
 router.get('/students/:studentId', getStudentById);
+router.post('/students', createStudent);
+router.delete('/students/:studentId', deleteStudent);
+router.patch('/students/:studentId', updateStudent);
 
 export default router;
